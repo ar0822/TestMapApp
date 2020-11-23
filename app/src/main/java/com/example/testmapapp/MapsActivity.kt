7e2ea99f17dbfec05c8a9f9afc739cf433be445b
Ryuji Nagae <ai.ryuji.0822@gmail.com>
2020-11-23 16:00:35 +0900

    ポリゴン作成コード記入

--- app/src/main/java/com/example/testmapapp/MapsActivity.kt
+++ app/src/main/java/com/example/testmapapp/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.testmapapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,10 +10,14 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polygon
+import com.google.android.gms.maps.model.PolygonOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private var locationList: ArrayList<LatLng> = ArrayList()
+    private var polygonList: ArrayList<Polygon> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        mMap!!.setOnMapClickListener { latlng ->
+            val location = LatLng(latlng.latitude, latlng.longitude)
+
+            if (locationList.size < 4) {
+                locationList.add(location)
+                mMap!!.addMarker(MarkerOptions().position(location))
+                if(locationList.size > 2) {
+                    if(polygonList.size > 0 && locationList.size == 4) polygonList[polygonList.size-1].remove()
+
+                    val polygon: Polygon = mMap!!.addPolygon(
+                            PolygonOptions()
+                                    .addAll(locationList)
+                                    .strokeColor(Color.RED)
+                                    .strokeWidth(3f)
+                                    .fillColor(R.color.polygonFill)
+                    )
+                    polygonList.add(polygon)
+                }
+                if(locationList.size == 4) locationList.clear()
+            }
+
+        }
+        locationList.clear()
+        polygonList.clear()
     }
 
     /**
@@ -36,7 +66,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        val sydney = LatLng(35.6724487, 139.769739)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
